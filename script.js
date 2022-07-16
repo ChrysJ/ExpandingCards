de15562a515eb16c4c5e95e0bde27f988437ba11
ChrysJ <jouin.chrystopher@gmail.com>
2022-07-16 15:39:00 +0200

    project 1 from 50 projects in 50 days

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let boxes = document.querySelectorAll('.boxes');
 
 boxes.forEach((box) => {
 	box.addEventListener('click', (e) => {
-		console.log(e.target);
 		removeClassBox();
 		e.target.classList.add('active');
 	});
